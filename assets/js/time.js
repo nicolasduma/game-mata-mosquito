4f1398ad54a8243102780d3ceb2dbfc10fde5313
Nicolas Rafael Duma <nicolasrduma98@outlook.com.br>
2021-05-04 14:56:35 -0300

    update time.js file

--- assets/js/time.js
+++ assets/js/time.js
@@ -2,7 +2,6 @@ function startTime() {
     let time = document.getElementById("time")
     let nTime = 100
     time.innerHTML = nTime
-    changeFly()
     setInterval(function () {
             nTime = nTime - 1
             time.innerHTML = nTime
